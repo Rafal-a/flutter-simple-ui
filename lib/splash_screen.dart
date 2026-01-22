e8ddaa465728a1022d03bd885dda942400626097
Rafal A <rafal.akeel99@gmail.com>
2026-01-22 21:39:27 +0300

    make splash screen stateful widget

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_ui/login_page.dart';
 import 'package:flutter_simple_ui/widgets/logo_text.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+    @override
+  State<SplashScreen> createState() => _SplashScreenState();
+  }
+
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 1),
+      ()=>Navigator.push(context,MaterialPageRoute(builder:   (context) => LoginPage())),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,4 +78,5 @@ class SplashScreen extends StatelessWidget {
       ],
     );
   }
+  
 }
